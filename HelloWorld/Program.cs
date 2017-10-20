a4b9ee87b7370ade62046a4618bfbd11490a69c2
johnvincentwest <johnvincentwest@gmail.com>
2017-10-20 04:30:17 -0700

    Added some first name

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			Console.WriteLine("What is your first name");
+			string name = Console.ReadLine();
+			Console.WriteLine("First name provided: " + name);
 		}
 	}
 }
